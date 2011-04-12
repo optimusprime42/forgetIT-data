ca528396e913b0f42d871a4323e377d48980fbb8
optimusprime42 <marc_brandner@web.de>
2011-04-12 19:50:30 +0200

    Transferred the getEntities() method from the Handler to the Provider.

--- forgetIT/src/forgetit/common/Main.java
+++ forgetIT/src/forgetit/common/Main.java
@@ -1,6 +1,13 @@
 package forgetit.common;
 
+import java.util.Iterator;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import forgetit.db.DBEntityHandler;
+import forgetit.db.DBEntityProvider;
 
 
 public class Main {
@@ -15,16 +22,25 @@ public class Main {
 		// display.dispose();
 
 		DBEntityHandler handler = new DBEntityHandler();
+		DBEntityProvider provider = new DBEntityProvider();
 		
-		/*Entity entityOne = new Entity();
+		/*
+		 * // Add a example tuple to the databse
+		Entity entityOne = new Entity();
 		entityOne.setDescription("Hello there!");
 		entityOne.setTitle("SomeTestEntity");
-		handler.addEntity(entityOne);*/
+		handler.addEntity(entityOne);
+		*/
 		
-		handler.listEntities();
-		
-	
-		
+		// Get all entities from the database and also list them
+		List<Entity> entities = provider.getEntities();
+		for (Iterator<Entity> iter = entities.iterator(); iter.hasNext();) {
+			Entity element = (Entity) iter.next();
+			System.out.println(element);
+			System.out.println(element.getId());
+			System.out.println(element.getTitle());
+			System.out.println(element.getDescription());
+		}
 		
 	}
 }
